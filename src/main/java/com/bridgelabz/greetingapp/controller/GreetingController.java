b5b4b83a6330d306e620e92319634167893aa4c5
23f3000161 <23f3000161@ds.study.iitm.ac.in>
2026-03-21 22:52:22 +0530

    [Devraj]:UC3 - Greeting with firstName and lastName support, dynamic greeting messages

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -15,10 +15,12 @@ public class GreetingController {
     private GreetingService greetingService;
 
     @GetMapping
-    public Map<String, String> getGreeting() {
+    public Map<String, String> getGreeting(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
         Map<String, String> response = new HashMap<>();
         response.put("method", "GET");
-        response.put("message", greetingService.getGreeting());
+        response.put("message", greetingService.getGreeting(firstName, lastName));
         return response;
     }
 
