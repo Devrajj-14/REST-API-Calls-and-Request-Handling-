c4570f4c01cb9c3f409e2d9661d8c56d67d5254b
23f3000161 <23f3000161@ds.study.iitm.ac.in>
2026-03-21 22:50:45 +0530

    [Devraj]:UC1 - GreetingController with GET, POST, PUT, DELETE HTTP methods returning JSON

---
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.greetingapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/greeting")
+public class GreetingController {
+
+    @GetMapping
+    public Map<String, String> getGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("method", "GET");
+        response.put("message", "Hello World");
+        return response;
+    }
+
+    @PostMapping
+    public Map<String, String> postGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("method", "POST");
+        response.put("message", "Hello World");
+        return response;
+    }
+
+    @PutMapping
+    public Map<String, String> putGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("method", "PUT");
+        response.put("message", "Hello World");
+        return response;
+    }
+
+    @DeleteMapping
+    public Map<String, String> deleteGreeting() {
+        Map<String, String> response = new HashMap<>();
+        response.put("method", "DELETE");
+        response.put("message", "Hello World");
+        return response;
+    }
+}
